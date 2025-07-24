31d82964d0ee3aa19a126bab44444cda5db08042
Melkizedeki <mmagota2@gmail.com>
2025-07-24 15:29:20 +0300

    The commit after using component and compent scanning to automatically search for all the components in the package

---
+++ src/main/java/scan/component/scanning/Employee.java
@@ -0,0 +1,56 @@
+package scan.component.scanning;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+
+    private  int employeeId;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
